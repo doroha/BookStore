34a98f376a801277aac56215d080de82664942c7
doroha <doroha@post.bgu.ac.il>
2018-11-27 20:45:19 +0200

    4 Commit

--- src/main/java/bgu/spl/mics/application/passiveObjects/OrderReceipt.java
+++ src/main/java/bgu/spl/mics/application/passiveObjects/OrderReceipt.java
@@ -8,21 +8,33 @@ package bgu.spl.mics.application.passiveObjects;
  * You may add fields and methods to this class as you see fit (including public methods).
  */
 public class OrderReceipt {
+	private int orderId;
+	private String seller;
+	private int customerId;
+	private String bookTitle;
+	private int price;
+
+	public OrderReceipt(int orderId, String seller, int customerId, String bookTitle, int price){
+		this.orderId=orderId;
+		this.seller=seller;
+		this.customerId=customerId;
+		this.bookTitle=bookTitle;
+		this.price=price;
+
+	}
 	
 	/**
      * Retrieves the orderId of this receipt.
      */
 	public int getOrderId() {
-		// TODO Implement this
-		return 0;
+		return orderId;
 	}
 	
 	/**
      * Retrieves the name of the selling service which handled the order.
      */
 	public String getSeller() {
-		// TODO Implement this
-		return null;
+		return seller;
 	}
 	
 	/**
@@ -31,24 +43,21 @@ public class OrderReceipt {
      * @return the ID of the customer
      */
 	public int getCustomerId() {
-		// TODO Implement this
-		return 0;
+		return customerId;
 	}
 	
 	/**
      * Retrieves the name of the book which was bought.
      */
 	public String getBookTitle() {
-		// TODO Implement this
-		return null;
+		return bookTitle;
 	}
 	
 	/**
      * Retrieves the price the customer paid for the book.
      */
 	public int getPrice() {
-		// TODO Implement this
-		return 0;
+		return price;
 	}
 	
 	/**
